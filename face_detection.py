c6f8563031b70b74c3b681a72035364f63714af5
SB299792458 <shikharbrajesh9@gmail.com>
2018-12-06 14:42:21 -0500

    one frame generated

--- face_detection.py
+++ face_detection.py
@@ -1,19 +1,20 @@
 import cv2
+import numpy as np
+import face_recognition
 from helpers import *
+import pdb
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+def landmark_detect(img):
+    face_landmarks_list = face_recognition.face_landmarks(img)
+    #pdb.set_trace()
+    values = []
+    for key, value in face_landmarks_list[0].items():
+        for ent in value:
+            values.append(ent)
 
-def face_detect(img):
-    global face_cascade
-    global eye_cascade
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = img[y:y + h, x:x + w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-    showBGRimage('img', img)
+    values = np.array(values).astype(np.int32)
+    #visualizeFeatures(img, (values[:, 0], values[:, 1]))
+    #values is 72*2
+
+    values = listOfListToTuples(values)
+    return values
--- main.py
+++ main.py
@@ -25,17 +25,68 @@ if __name__ == "__main__":
 		while True:
 			flag_source, source_frame = cap_source.read()
 			flag_target, target_frame = cap_target.read()
+			img1Warped = np.copy(target_frame);
 
 			if flag_source and flag_target:
 				pos_frame = cap_source.get(cv2.CAP_PROP_POS_FRAMES)
 				if pos_frame == 1:
-					showBGRimage(source_frame)
+					#showBGRimage(source_frame)
 					#showBGRimage(target_frame)
-					face_detection.face_detect(source_frame)
+					points1 = face_detection.landmark_detect(source_frame)
+					points2 = face_detection.landmark_detect(target_frame)
+
+					# Find convex hull
+					hull1 = []
+					hull2 = []
+
+					hullIndex = cv2.convexHull(np.array(points2), returnPoints=False)
+
+					for i in xrange(0, len(hullIndex)):
+						hull1.append(points1[int(hullIndex[i])])
+						hull2.append(points2[int(hullIndex[i])])
+					#visualizeFeatures(source_frame, hull1)
+					# Find delanauy traingulation for convex hull points
+					sizeImg2 = target_frame.shape
+					rect = (0, 0, sizeImg2[1], sizeImg2[0])
+
+					dt = calculateDelaunayTriangles(rect, hull2)
+
+					if len(dt) == 0:
+						quit()
+
+					# Apply affine transformation to Delaunay triangles
+					for i in xrange(0, len(dt)):
+						t1 = []
+						t2 = []
+
+						# get points for img1, img2 corresponding to the triangles
+						for j in xrange(0, 3):
+							t1.append(hull1[dt[i][j]])
+							t2.append(hull2[dt[i][j]])
+
+						warpTriangle(source_frame, img1Warped, t1, t2)
+
+					# Calculate Mask
+					hull8U = []
+					for i in xrange(0, len(hull2)):
+						hull8U.append((hull2[i][0], hull2[i][1]))
+
+					mask = np.zeros(target_frame.shape, dtype=target_frame.dtype)
+
+					cv2.fillConvexPoly(mask, np.int32(hull8U), (255, 255, 255))
+
+					r = cv2.boundingRect(np.float32([hull2]))
+
+					center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
+
+					# Clone seamlessly.
+					output = cv2.seamlessClone(np.uint8(img1Warped), target_frame, mask, center, cv2.NORMAL_CLONE)
+					showBGRimage(output)
+					cv2.imshow("Face Swapped", output)
 					#out.write(input_frame)
 				else:
 					break
-				print str(pos_frame) + " frames"
+					print str(pos_frame) + " frames"
 			else:
 				if flag_source:
 					videoSpecific2(cap_source, pos_frame)
