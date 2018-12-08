e3b9d02ef9c8309975869c0858d2645578c2e4e5
SB299792458 <shikharbrajesh9@gmail.com>
2018-12-08 14:07:17 -0500

    normal delaunay

--- triangulation.py
+++ triangulation.py
@@ -1,16 +1,22 @@
 import cv2, numpy as np
 import random
 from helpers import *
+from scipy import spatial
 
 def triangulation(target_frame, hull2):
     # Find delanauy traingulation for convex hull points
     sizeImg2 = target_frame.shape
     rect = (0, 0, sizeImg2[1], sizeImg2[0])
 
-    dt = calculateDelaunayTriangles(rect, hull2, target_frame)
+    dt = calculateDelaunayTriangles_spatial(rect, hull2, target_frame)
 
     return dt
 
+def calculateDelaunayTriangles_spatial(rect, points, img):
+	Tri = spatial.Delaunay(points)
+	triangles = Tri.simplices
+
+	return listOfListToTuples(triangles.tolist())
 
 # calculate delanauy triangle
 def calculateDelaunayTriangles(rect, points, img):
