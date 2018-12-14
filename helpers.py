100e7cfc379712e0d583f99e5cafff1c6da2fd9d
SB299792458 <shikharbrajesh9@gmail.com>
2018-12-14 14:16:34 -0500

    fix

--- helpers.py
+++ helpers.py
@@ -63,10 +63,10 @@ def draw_delaunay(img, subdiv, delaunay_color = (255, 255, 255)) :
 		pt2 = (t[2], t[3])
 		pt3 = (t[4], t[5])
 
-	if rectContains(r, pt1) and rectContains(r, pt2) and rectContains(r, pt3) :
-		cv2.line(img, pt1, pt2, delaunay_color, 1, cv2.LINE_AA, 0)
-		cv2.line(img, pt2, pt3, delaunay_color, 1, cv2.LINE_AA, 0)
-		cv2.line(img, pt3, pt1, delaunay_color, 1, cv2.LINE_AA, 0)
+		if rectContains(r, pt1) and rectContains(r, pt2) and rectContains(r, pt3) :
+			cv2.line(img, pt1, pt2, delaunay_color, 1, cv2.LINE_AA, 0)
+			cv2.line(img, pt2, pt3, delaunay_color, 1, cv2.LINE_AA, 0)
+			cv2.line(img, pt3, pt1, delaunay_color, 1, cv2.LINE_AA, 0)
 
 
 def visualizeFeatures(colorImg, p):
